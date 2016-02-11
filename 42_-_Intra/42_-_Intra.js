e2946ce02371357f2fcfcc2954e961ff6f4c9f62
Aymeric MAITRE <amaitre@e2r4p13.42.fr>
2016-02-11 17:48:06 +0100

    MAJ 42 intra

--- 42_-_Intra/42_-_Intra.js
+++ 42_-_Intra/42_-_Intra.js
@@ -2,12 +2,11 @@
 // @name        42 - Intra
 // @namespace   https://greasyfork.org/fr/users/11667-aymeric-maitre
 // @description 42 - Intra
-// @include     https://profile.intra.42.fr/
+// @include     https://profile.intra.42.fr/*
 // @version     1
 // @grant       none
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // ==/UserScript==
 
-var block = $('.row')[2];
+var block = document.getElementsByClassName('row')[document.getElementsByClassName('row').length - 1];
 var link = "https://www.youtube.com/embed/deoseries?list=PLGpavwBxqIn0IvuGVYSkvwlZezS7ZqEpE";
 block.innerHTML = '<div class="col-lg-4 col-md-6 col-xs-12 fixed-height"><div class="container-inner-item boxed">	<h4 class="profile-title">		Youtube Playlist	</h4>	<iframe width="700" height="315" src="' + link + '" frameborder="0" lowfullscreen></iframe></div></div>' + block.innerHTML;
