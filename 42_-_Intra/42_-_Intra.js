b00d8eb080c9ce1085290b58cb3dd8ffa8c6ed09
Aymeric MAITRE <amaitre@e2r5p10.42.fr>
2016-02-12 15:29:15 +0100

    42 intra V2

--- 42_-_Intra/42_-_Intra.js
+++ 42_-_Intra/42_-_Intra.js
@@ -3,7 +3,7 @@
 // @namespace   https://greasyfork.org/fr/users/11667-aymeric-maitre
 // @description Ajoute une playlist youtube sur l'intra de l'ecole 42 https://profile.intra.42.fr/
 // @include     https://profile.intra.42.fr/*
-// @version     1.1
+// @version     2
 // @icon        https://profile.intra.42.fr/assets/42_logo_black-684989d43d629b3c0ff6fd7e1157ee04db9bb7a73fba8ec4e01543d650a1c607.png
 // @grant       none
 // ==/UserScript==
@@ -11,3 +11,10 @@
 var block = document.getElementsByClassName('row')[document.getElementsByClassName('row').length - 1];
 var link = "https://www.youtube.com/embed/deoseries?list=PLGpavwBxqIn0IvuGVYSkvwlZezS7ZqEpE";
 block.innerHTML = '<div class="col-lg-4 col-md-6 col-xs-12 fixed-height"><div class="container-inner-item boxed">	<h4 class="profile-title">		Youtube Playlist	</h4>	<iframe width="700" height="315" src="' + link + '" frameborder="0" lowfullscreen></iframe></div></div>' + block.innerHTML;
+
+var event = document.getElementsByClassName('event-item');
+for each (var item in event) {
+  if (item.getElementsByClassName('event-full')[0].innerHTML != undefined)
+    item.style.display = "none";
+
+}
